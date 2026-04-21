6c6cccacc16970cd3c9c0f1bc2d6705f95b0c321
Ljhkss <l3369671920@outlook.com>
2026-04-21 10:22:24 +0800

    Accomplish the lab04

--- labs/lab04/ex1.s
+++ labs/lab04/ex1.s
@@ -1,4 +1,4 @@
-.globl pow inc_arr
+.globl pow inc_arr helper_fn
 
 .data
 fail_message: .asciiz "%s test failed\n"
@@ -49,7 +49,8 @@ next_test:
 #
 pow:
     # BEGIN PROLOGUE
-    # FIXME: Need to save the callee saved register(s)
+    addi sp, sp, -4
+    sw s0, 0(sp)
     # END PROLOGUE
     li s0, 1
 pow_loop:
@@ -60,7 +61,8 @@ pow_loop:
 pow_end:
     mv a0, s0
     # BEGIN EPILOGUE
-    # FIXME: Need to restore the callee saved register(s)
+    lw s0, 0(sp)
+    addi sp, sp, 4
     # END EPILOGUE
     jr ra
 
@@ -73,8 +75,10 @@ pow_end:
 inc_arr:
     # BEGIN PROLOGUE
     # FIXME: What other registers need to be saved?
-    addi sp, sp, -4
+    addi sp, sp, -12
     sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
     # END PROLOGUE
     mv s0, a0 # Copy start of array to saved register
     mv s1, a1 # Copy length of array to saved register
@@ -88,16 +92,24 @@ inc_arr_loop:
     # FIXME: Add code to preserve the value in t0 before we call helper_fn
     # Also ask yourself this: why don't we need to preserve t1?
     #
+    addi sp, sp, -4
+    sw t0, 0(sp)
+
     jal ra helper_fn
     # FIXME: Restore t0
     # Finished call for helper_fn
+    lw t0, 0(sp)
+    addi sp, sp, 4
+
     addi t0, t0, 1 # Increment counter
     j inc_arr_loop
 inc_arr_end:
     # BEGIN EPILOGUE
     # FIXME: What other registers need to be restored?
     lw ra, 0(sp)
-    addi sp, sp, 4
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    addi sp, sp, 12
     # END EPILOGUE
     jr ra
 
@@ -111,13 +123,15 @@ inc_arr_end:
 # as appropriate.
 helper_fn:
     # BEGIN PROLOGUE
-    # FIXME: YOUR CODE HERE
+    addi sp, sp, -4
+    sw s0, 0(sp)
     # END PROLOGUE
     lw t1, 0(a0)
     addi s0, t1, 1
     sw s0, 0(a0)
     # BEGIN EPILOGUE
-    # FIXME: YOUR CODE HERE
+    lw s0, 0(sp)
+    addi sp, sp, 4
     # END EPILOGUE
     jr ra
 
--- labs/lab04/ex3.s
+++ labs/lab04/ex3.s
@@ -1,6 +1,6 @@
 .data
-n: .word 2
-exp: .word 10
+n: .word 5
+exp: .word 3
 
 .text
 main:
@@ -32,7 +32,9 @@ main:
 #     where ^ is the exponent operator, not XOR
 ex3:
     # Note: Add code BELOW without altering existing lines.
-
+    addi sp, sp, -8
+    sw t0, 0(sp)
+    sw ra, 4(sp)
     # return 1 if a1 == 0
     beq a1 x0 ex3_zero_case
 
@@ -53,4 +55,7 @@ ex3_zero_case:
     li a0 1
 
 ex3_end:
+    lw t0, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8
     jr ra
